7b051aa5c53fc262ad115797b7ca58ee0a62122c
Louis Canning <loujamcan@btinternet.com>
2025-08-19 00:56:21 +0100

    to synthesise

--- source/tree_node.sv
+++ source/tree_node.sv
@@ -29,7 +29,7 @@
 
 
 
-module  tree_node #(parameter int rows,int width, int struct_array [0:rows-1][0:width-1], int splits [0:rows-1], int layer, int output_width, int input_width)
+module  tree_node #(parameter int rows = 8,int width = 10, int struct_array [0:rows-1][0:width-1] = '{{0,0,15,14,1,1,3,2,-1,-1},{1,0,1,0,0,3,1,4,2,5},{2,0,3,0,8,6,1,7,-1,-1}, {3,1,0,-1,-1,-1,-1,-1,-1,-1}, {4,1,1,-1,-1,-1,-1,-1,-1,-1}, {5,1,2,-1,-1,-1,-1,-1,-1,-1}, {6,1,24,-1,-1,-1,-1,-1,-1,-1}, {7,1,3,-1,-1,-1,-1,-1,-1,-1}}, int splits [0:rows-1] = '{2,3,2,1,1,1,1,1}, int layer = 0, int output_width = 16, int input_width = 16)
 
 (
     input [input_width-1:0] inputs,
